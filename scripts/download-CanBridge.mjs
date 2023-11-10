462adaaa29398aedbcfce0173ced70323d656dee
NoahAndrews <noah@revrobotics.com>
2023-11-10 10:48:39 -0600

    Update CANBridge to 2.2.0

--- scripts/download-CanBridge.mjs
+++ scripts/download-CanBridge.mjs
@@ -3,7 +3,7 @@ import * as path from "path";
 import axios from 'axios';
 import AdmZip from 'adm-zip';
 
-const canBridgeTag = "v2.1.0";
+const canBridgeTag = "v2.2.0";
 const canBridgeReleaseAssetUrlPrefix = `https://github.com/REVrobotics/CANBridge/releases/download/${canBridgeTag}`;
 
 const externalCompileTimeDepsPath = 'externalCompileTimeDeps';
