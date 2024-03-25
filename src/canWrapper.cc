6f692b81ebfc056a34c1421847a2ac6cd08ec241
NoahAndrews <noah@revrobotics.com>
2024-03-25 16:50:09 -0500

    Check for expired heartbeats more frequently

--- src/canWrapper.cc
+++ src/canWrapper.cc
@@ -648,7 +648,7 @@ void writeDfuToBin(const Napi::CallbackInfo& info) {
 
 void heartbeatsWatchdog() {
     while (true) {
-        std::this_thread::sleep_for (std::chrono::seconds(1));
+        std::this_thread::sleep_for (std::chrono::milliseconds(250));
 
         {
             // Erase removed CAN buses from heartbeatsRunning
