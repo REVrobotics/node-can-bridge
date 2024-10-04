07b288117fbfa2d5dc0c9dc14c1ede59714630e3
LandryNorris <landry.norris@live.com>
2024-10-04 17:15:43 -0500

    Add new parameter to typescript definition

--- lib/binding.ts
+++ lib/binding.ts
@@ -64,7 +64,7 @@ export class CanBridge {
     initializeNotifier: () => void;
     waitForNotifierAlarm: (time:number) => Promise<number>;
     stopNotifier: () => void;
-    writeDfuToBin: (dfuFileName:string, elementIndex: number, binFileName:string) => Promise<number>;
+    writeDfuToBin: (dfuFileName:string, binFileName:string, elementIndex: number) => Promise<number>;
     getImageElements: (dfuFileName: string, imageIndex: number) => DfuImageElement[];
     openHALStreamSession: (messageId: number, messageMask:number, numMessages:number) => number;
     readHALStreamSession: (streamHandle:number, numMessages:number) => CanMessage[];
