e56c612aa95cd3e80212881fbd248717d1c19a10
NoahAndrews <10224994+NoahAndrews@users.noreply.github.com.>
2024-11-10 13:20:05 -0600

    Static linking experiment

--- move-files.cmake
+++
@@ -1,6 +0,0 @@
-
-function (create_move_target name src dest)
-    if(EXISTS ${src})
-        configure_file(${src} ${dest} COPYONLY)
-    endif()
-endfunction()
--- scripts/download-CanBridge.mjs
+++ scripts/download-CanBridge.mjs
@@ -7,25 +7,31 @@ const canBridgeTag = "v2.5.1";
 const canBridgeReleaseAssetUrlPrefix = `https://github.com/REVrobotics/CANBridge/releases/download/${canBridgeTag}`;
 
 const externalCompileTimeDepsPath = 'externalCompileTimeDeps';
-const runtimeArtifactsPath = path.join('prebuilds', 'node_canbridge-win32-x64');
 const tempDir = 'temp';
+const headersZipFilename = 'headers.zip';
+const wpiHalWindowsZipFilename = 'wpihal-windows-x64.zip';
+const wpiUtilWindowsZipFilename = 'wpiutil-windows-x64.zip';
 
 try {
+    const wpiHalVersion = (await axios.get(`${canBridgeReleaseAssetUrlPrefix}/wpiHalVersion.txt`, { responseType: "text"})).data;
+    console.log(`WPILib HAL version: ${wpiHalVersion}`)
     await Promise.all(Array.of(
-        downloadCanBridgeArtifact('CANBridge.lib', externalCompileTimeDepsPath),
-        downloadCanBridgeArtifact('CANBridge.dll', runtimeArtifactsPath),
-        downloadCanBridgeArtifact('wpiHal.lib', externalCompileTimeDepsPath),
-        downloadCanBridgeArtifact('wpiHal.dll', runtimeArtifactsPath),
-        downloadCanBridgeArtifact('wpiutil.lib', externalCompileTimeDepsPath),
-        downloadCanBridgeArtifact('wpiutil.dll', runtimeArtifactsPath),
-        downloadCanBridgeArtifact('headers.zip', tempDir),
+        downloadCanBridgeArtifact('CANBridge-static.lib', externalCompileTimeDepsPath, "CANBridge.lib"),
+        downloadCanBridgeArtifact(headersZipFilename, tempDir),
+        downloadFile(`https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/hal/hal-cpp/${wpiHalVersion}/hal-cpp-${wpiHalVersion}-windowsx86-64static.zip`, tempDir, wpiHalWindowsZipFilename),
+        downloadFile(`https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/wpiutil/wpiutil-cpp/${wpiHalVersion}/wpiutil-cpp-${wpiHalVersion}-windowsx86-64static.zip`, tempDir, wpiUtilWindowsZipFilename),
     ));
 
     console.log("Extracting headers");
+    new AdmZip(path.join(tempDir, headersZipFilename))
+        .extractAllTo(path.join(externalCompileTimeDepsPath, 'include'), true);
+    console.log("Extracting WPILib HAL");
+    new AdmZip(path.join(tempDir, wpiHalWindowsZipFilename))
+        .extractEntryTo("windows/x86-64/static/wpiHal.lib", externalCompileTimeDepsPath, false, true);;
+    console.log("Extracting WPIUtil");
+    new AdmZip(path.join(tempDir, wpiUtilWindowsZipFilename))
+        .extractEntryTo("windows/x86-64/static/wpiutil.lib", externalCompileTimeDepsPath, false, true);
 
-    const headersZip = new AdmZip(path.join(tempDir, "headers.zip"));
-
-    await headersZip.extractAllTo(path.join(externalCompileTimeDepsPath, 'include'), true);
     console.log(`Successfully downloaded CANBridge ${canBridgeTag}`);
 } catch (e) {
     if (axios.isAxiosError(e) && e.request) {
@@ -42,10 +48,34 @@ try {
     }
 }
 
-async function downloadCanBridgeArtifact(filename, destDir) {
+/**
+ *
+ * @param {string} filename
+ * @param {string} destDir
+ * @param {string} [destFilename]
+ * @returns {Promise<undefined>}
+ */
+async function downloadCanBridgeArtifact(filename, destDir, destFilename) {
     fs.mkdirSync(destDir, { recursive: true });
     const response = await axios.get(`${canBridgeReleaseAssetUrlPrefix}/${filename}`, { responseType: "stream" });
-    const fileStream = fs.createWriteStream(`${destDir}/${filename}`);
+    const fileStream = fs.createWriteStream(`${destDir}/${destFilename || filename}`);
+    response.data.pipe(fileStream);
+    await new Promise(resolve => {
+        fileStream.on('finish', resolve);
+    });
+}
+
+/**
+ *
+ * @param {string} url
+ * @param {string} destDir
+ * @param {string} destFilename
+ * @returns {Promise<undefined>}
+ */
+async function downloadFile(url, destDir, destFilename) {
+    fs.mkdirSync(destDir, { recursive: true });
+    const response = await axios.get(url, { responseType: "stream" });
+    const fileStream = fs.createWriteStream(`${destDir}/${destFilename}`);
     response.data.pipe(fileStream);
     await new Promise(resolve => {
         fileStream.on('finish', resolve);
